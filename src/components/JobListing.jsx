582df89b76b4340cbbcf7fde6f29d73bec612a3f
BrianXYZ <brianchima22@gmail.com>
2024-10-23 15:14:04 +0100

    added react-icon, fixed map marker icon

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 const JobListing = ({ type, title, description, location, salary, id }) => {
     const [showFull, setShowFull] = useState(false);
@@ -29,7 +30,7 @@ const JobListing = ({ type, title, description, location, salary, id }) => {
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                        <i className="fa-solid fa-location-dot text-lg"></i>
+                        <FaMapMarker className="inline text-lg mb-1 mr-1" />
                         {location}
                     </div>
                     <a
