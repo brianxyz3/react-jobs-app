d95ac565cff6b5d1bc30a675dbb69564c814d21d
BrianXYZ <brianchima22@gmail.com>
2024-10-23 17:57:31 +0100

    added react route, layout and homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import Hero from "./components/Hero";
-import NavBar from "./components/NavBar";
-import HomeCards from "./components/HomeCards";
-import JobList from "./components/JobList";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  ));
 
 function App() {
-
-  return (
-    <>
-
-      <NavBar />
-      {/* <!-- Hero --> */}
-      <Hero />
-
-      {/* <!-- Developers and Employers --> */}
-      <HomeCards />
-
-
-      {/* <!-- Browse Jobs --> */}
-      <JobList />
-
-      <ViewAllJobs />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
