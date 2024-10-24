ab44bab12fa9c2d5a33906af4fa352293bd20476
BrianXYZ <brianchima22@gmail.com>
2024-10-24 11:20:26 +0100

    added mock api with json-server, added loading spinner

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,22 +1,45 @@
-import jobs from "../jobs.json";
+import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobList = ({ isHome = false }) => {
-    const last3Jobs = jobs.length - 3;
-    const jobList = isHome ? jobs.slice(last3Jobs, jobs.length) : jobs;
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const apiUrl = isHome ? "http://localhost:5000/jobs?_limit=3" : "http://localhost:5000/jobs"
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                return setJobs(data);
+            } catch (error) {
+                return console.log("Error fetching data, " + error);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchJobs()
+    }, [])
+
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? "Recent Jobs" : "Browse Jobs"}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobList.map((job) => {
-                        return (
-                            <JobListing key={job.id} type={job.type} title={job.title} description={job.description} salary={job.salary} location={job.location} id={job.id} />
-                        )
-                    })}
-                </div>
+                {loading ? (<Spinner loading={loading} />) :
+                    (
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                            {jobs.map((job) => {
+                                return (
+                                    <JobListing key={job.id} type={job.type} title={job.title} description={job.description} salary={job.salary} location={job.location} id={job.id} />
+                                )
+                            })}
+                        </div>
+                    )
+                }
             </div>
         </section>
     )
