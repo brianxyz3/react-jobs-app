72b2737fbc3330ea6359decfbbb4cae31ac3a9f1
BrianXYZ <brianchima22@gmail.com>
2024-10-30 20:50:41 +0100

    added utilities and backend error handling

--- jobs-api/index.js
+++ jobs-api/index.js
@@ -5,6 +5,8 @@ if (process.env.NODE_ENV !== "production") {
 }
 import express from "express";
 import mongoose from "mongoose";
+import ExpressError from "./utilities/ExpressError.js";
+import catchAsync from "./utilities/catchAsync.js";
 import Job from "./model/job.js";
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/react-jobs";
@@ -24,57 +26,63 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/jobs", async (req, res) => {
-  try {
+app.get(
+  "/jobs",
+  catchAsync(async (req, res) => {
     console.log(req);
     const jobs = await Job.find();
     res.status(200).json(jobs);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+  })
+);
 
-app.post("/jobs", async (req, res) => {
-  try {
+app.post(
+  "/jobs",
+  catchAsync(async (req, res) => {
+    if (!req.body) throw new ExpressError(400, "Invalid Job Data");
     const newJob = new Job(req.body);
+    if (!newJob) throw ExpressError(400, "Invalid Job Data Input(s)");
     await newJob.save();
     res.status(201).json(newJob);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+  })
+);
 
-app.get("/jobs/:id", async (req, res) => {
-  try {
+app.get(
+  "/jobs/:id",
+  catchAsync(async (req, res) => {
     const { id } = req.params;
     const job = await Job.findById(id);
-    if (!job) return res.status(404).json({ message: "Job not found" });
+    if (!job) throw new ExpressError(404, "Job Not Found");
     res.status(200).json(job);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+  })
+);
 
-app.put("/jobs/:id", async (req, res) => {
-  try {
+app.put(
+  "/jobs/:id",
+  catchAsync(async (req, res) => {
     const { id } = req.params;
     const updatedJob = await Job.findByIdAndUpdate(id, req.body);
-    if (!updatedJob) return res.status(404).json({ message: "Job not found" });
+    if (!updatedJob) throw new ExpressError(404, "Job Not Found");
     res.status(200).json(updatedJob);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+  })
+);
 
-app.delete("/jobs/:id", async (req, res) => {
-  try {
+app.delete(
+  "/jobs/:id",
+  catchAsync(async (req, res) => {
     const { id } = req.params;
     const deletedJob = await Job.findByIdAndDelete(id);
-    if (!deletedJob) return res.status(404).json({ message: "Job not found" });
-    res.status(200).json({ message: "Task deleted successfully" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    if (!deletedJob) throw new ExpressError(404, "Job Not Found");
+    res.status(200).json({ message: "Job deleted successfully" });
+  })
+);
+
+app.all("*", (req, res, next) => {
+  throw new ExpressError(404, "Page Not Found");
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something Went Wrong. Try Again" } = err;
+  res.status(statusCode).json(`${statusCode} ${message}`);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
+    const apiUrl = "/api/*";
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        const getJobs = async () => {
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                return setError(data);
+            } catch (err) {
+                return console.log("Error fetching data, " + err);
+            }
+        }
+        getJobs()
+    }, [])
     return (
-        <section class="text-center flex flex-col justify-center items-center h-96">
-            {/* <i class="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"></i> */}
+        <section className="text-center flex flex-col justify-center items-center h-96">
+            {/* <i className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"></i> */}
             <FaExclamationTriangle className="text-yellow-400 text-6xl mb-4" />
-            <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
-            <p class="text-xl mb-5">This page does not exist</p>
+            <h1 className="text-6xl font-bold mb-4">{error}</h1>
+            <p className="text-xl mb-5">This page does not exist</p>
             <Link
                 to="/"
-                class="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
+                className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
             >Go Back
             </Link>
         </section>
