ae5d03e57737c4a474fa2f491468a6c76e9199aa
BrianXYZ <brianchima22@gmail.com>
2024-11-06 11:01:43 +0100

    added login, signup and logout link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/logo.png";
+import ConfirmPopup from "./ConfirmPopup";
 
 const NavBar = () => {
+    const [showPopup, setShowPopup] = useState(false);
+    const navigate = useNavigate();
+
+
+
+    const togglePopup = () => { setShowPopup(prevState => !prevState) }
+    const logout = () => {
+        localStorage.clear();
+        togglePopup();
+        navigate(0)
+        toast.success("Logout Successfully, Goodbye");
+    }
+    const cancel = () => {
+        togglePopup();
+        toast.error("Cancelled Logout");
+    }
     const checkActive = ({ isActive }) => {
         if (isActive) {
             return "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
@@ -11,6 +31,7 @@ const NavBar = () => {
     }
     return (
         <nav className="bg-indigo-700 border-b border-indigo-500">
+            {showPopup && <ConfirmPopup onConfirm={logout} onCancel={cancel} text="Logout" />}
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="flex h-20 items-center justify-between">
                     <div
@@ -37,10 +58,20 @@ const NavBar = () => {
                                     to="/jobs"
                                     className={checkActive}
                                 >Jobs</NavLink>
-                                <NavLink
+                                {localStorage.token ?
+                                    <><NavLink
                                     to="/add-job"
                                     className={checkActive}
-                                >Add Job</NavLink>
+                                    >Add Job</NavLink>
+                                        <NavLink
+                                            className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                            onClick={togglePopup}>
+                                            Logout
+                                        </NavLink>
+                                    </> : <>
+                                        <NavLink to="/register" className={checkActive}>Sign Up</NavLink>
+                                        <NavLink to="/login" className={checkActive}>Login</NavLink>
+                                    </>}
                             </div>
                         </div>
                     </div>
