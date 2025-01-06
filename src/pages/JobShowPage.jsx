a2096b6520d48068e16e67bb1e6c77dfbb0b5f7a
BrianXYZ <brianchima22@gmail.com>
2025-01-06 17:31:59 +0100

    fixed frontend authorization logic: allows unauthenticated user to view each job, checks if authententicated before check if authorized

--- src/pages/JobShowPage.jsx
+++ src/pages/JobShowPage.jsx
@@ -55,7 +55,7 @@ const JobShowPage = ({ deleteJob }) => {
                         <aside>
                             <CompanyInfo company={job.company} contact={job.contact} />
                             {/* Edit Job */}
-                            {currentUser.uid === job.postedBy && <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                            {currentUser && currentUser.uid === job.postedBy && <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                                 <Link
                                     to={`/edit-job/${job._id}`}
