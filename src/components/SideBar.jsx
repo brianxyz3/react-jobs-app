fa78a88698fe7d4485a0e52b37c397cc093de352
BrianXYZ <brianchima22@gmail.com>
2025-01-11 21:31:51 +0100

    fixed display user display name bug and moved login and logout auth function to side bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import ConfirmPopup from "./ConfirmPopup";
+import { doLogOut } from "../../controllers/auth";
+import { toast } from "react-toastify";
 import logo from "../assets/images/logo.png";
-import { CalendarMonthOutlined, DashboardOutlined, HomeOutlined, HourglassBottomOutlined, MenuOpen, AccountCircle, Logout } from "@mui/icons-material";
+import { CalendarMonthOutlined, DashboardOutlined, HomeOutlined, HourglassBottomOutlined, MenuOpen, AccountCircle, Logout, Login } from "@mui/icons-material";
+import { useAuth } from "../firebaseContext/authContext";
 
 
 const SideBar = ({ showSideBar, toggleSideBar }) => {
+    const [showLogOutPopup, setShowLogOutPopup] = useState(false);
+    const { currentUser, userLoggedIn } = useAuth();
+
 
     const menuItems = [
         {
@@ -22,13 +30,32 @@ const SideBar = ({ showSideBar, toggleSideBar }) => {
             label: "Schedule",
             icon: <CalendarMonthOutlined sx={{ fontSize: 30 }} />
         },
-    ]
+    ];
+
+    const toggleLogOutPopup = () => { setShowLogOutPopup(prevState => !prevState) };
+
+
+    const logOut = async () => {
+        doLogOut();
+        await cookieStore.delete("userId");
+        toggleLogOutPopup();
+        toast.success("Logout Successful, Goodbye");
+    }
+
+    const cancel = () => {
+        toggleLogOutPopup();
+        toast.error("Cancelled Logout");
+
+    };
 
     return (
-        <nav className={`${showSideBar ? "w-60 fixed" : "w-14 sticky"} top-0 items-center overflow-hidden shadow-md py-4 px-2 text-white bg-indigo-900 z-10 h-screen flex flex-col justify-between duration-300`}>
+        <nav className={`${showSideBar ? "max-w-60 fixed min-w-14" : "w-14 sticky"} top-0 items-center overflow-hidden shadow-md py-4 px-2 text-white bg-indigo-900 z-10 max-h-screen min-h-full flex flex-col justify-between duration-300`}>
+            {showLogOutPopup && <ConfirmPopup onConfirm={logOut} onCancel={cancel} text="Logout" />}
             <section className="w-full mt-2">
+                {/* Header */}
                 <div className="flex justify-between">
-                    {showSideBar && <NavLink className="" to="/">
+                    {/* Logo */}
+                    {showSideBar && <NavLink to="/">
                         <img
                             className="size-7"
                             src={logo}
@@ -48,11 +75,11 @@ const SideBar = ({ showSideBar, toggleSideBar }) => {
             </section>
             <div className="flex items-center w-full gap-3 cursor-pointer">
                 <div><AccountCircle sx={{ fontSize: 30 }} /></div>
-                <div className={`${!showSideBar && "translate-y-14"} duration-300 text-sm`}>
-                    <p>Dave</p>
-                    <p>dave@gmail.com</p>
+                <div className={`${!showSideBar && "translate-y-14"} duration-300 text-sm text-gray-400`}>
+                    <p>{userLoggedIn && currentUser.displayName || "John Doe"}</p>
+                    <p>{currentUser && currentUser.email}</p>
                 </div>
-                <div className="ms-auto"><Logout sx={{ fontSize: 24 }} className={`${!showSideBar && "translate-x-12"} duration-300 text-sm`} /></div>
+                {<div className={`${!showSideBar && "translate-x-12"} duration-300 text-sm ms-auto`}>{userLoggedIn ? <Logout sx={{ fontSize: 28 }} onClick={toggleLogOutPopup} /> : <NavLink to="/login"><Login sx={{ fontSize: 28 }} /></NavLink>}</div>}
             </div>
         </nav>
     )
