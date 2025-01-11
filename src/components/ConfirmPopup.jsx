af3c8571fea8954daea0f491ac44a495431cac14
BrianXYZ <brianchima22@gmail.com>
2025-01-11 21:32:58 +0100

    fixed confirmation popuop ui bug

--- src/components/ConfirmPopup.jsx
+++ src/components/ConfirmPopup.jsx
@@ -3,10 +3,12 @@ const ConfirmPopup = ({ id, onConfirm, onCancel, text }) => {
         <div style={{ background: "rgba(0,0,0,0.8)", zIndex: "10" }} className="text-center blur-none fixed inset-0 flex justify-center items-center p-3">
             <div className="bg-indigo-500 p-10 rounded-md">
                 <h3 className="font-bold text-xl mb-2">Are You Sure You Want To {text}?</h3>
+                <div className="flex justify-center gap-5">
                 <button onClick={() => onConfirm(id)}
-                    className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 mx-1 rounded-full focus:outline-none focus:shadow-outline mt-4 w-full md:w-auto">{text}</button>
+                        className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-5 rounded-full focus:outline-none focus:shadow-outline mt-4">{text}</button>
                 <button onClick={onCancel}
-                    className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 mx-1 rounded-full focus:outline-none focus:shadow-outline mt-4 w-full md:w-auto w-full md:w-auto">Cancel</button>
+                        className="bg-red-500 hover:bg-red-600 text-white font-bold py-3 px-5 rounded-full focus:outline-none focus:shadow-outline mt-4">Cancel</button>
+                </div>
             </div>
         </div>
     )
